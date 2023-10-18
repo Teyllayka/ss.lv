0e7f32324413c0159821f5613edb00d8a8b1af11
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-10-18 13:53:03 +0000

    hz

--- frontend/src/views/CreateAdvert.vue
+++ frontend/src/views/CreateAdvert.vue
@@ -108,32 +108,98 @@ export default {
 </script>
 
 <template>
-    <div>create advert</div>
-    <div class="input-field">
-        <input type="text" name="" id="text" v-model="v$.form.price.$model" placeholder="Price">
+    <div class="title"><h1>Create Advert:</h1></div>
+    <div class="fields">
+      <div class="input">
+      <div class="input-field">
+          <input type="text" name="" id="text" v-model="v$.form.price.$model" placeholder="Price">
+      </div>
+      <div class="input-field">
+          <input type="text" name="" id="location" v-model="v$.form.location.$model" placeholder="Location">
+      </div>
+      <div class="input-field">
+          <input type="text" name="" id="title" v-model="v$.form.title.$model" placeholder="Title">
+      </div>
+      <div class="input-field">
+          <input type="text" name="" id="description" v-model="v$.form.description.$model" placeholder="Description">
+      </div>
+      
+      
+     
     </div>
     <div class="input-field">
-        <input type="text" name="" id="location" v-model="v$.form.location.$model" placeholder="Location">
+      Category:
+        <select v-model="v$.form.category.$model">
+          <option v-for="category in enumerations" :key="category" :value="category">
+            {{ category }}
+          </option>
+        </select>
     </div>
-    <div class="input-field">
-        <input type="text" name="" id="title" v-model="v$.form.title.$model" placeholder="Title">
-    </div>
-    <div class="input-field">
-        <input type="text" name="" id="description" v-model="v$.form.description.$model" placeholder="Description">
-    </div>
-    <div class="input-field">
-      <select v-model="v$.form.category.$model">
-        <option v-for="category in enumerations" :key="category" :value="category">
-          {{ category }}
-        </option>
-      </select>
-    </div>
-    <div class="input-field" v-for="field in selectedCategoryFields" :key="field">
-      <input type="text" v-model="form[field]" :placeholder="field">
-      <div v-if="v$.form[field].$error">
-        This field is required.
+    <div class="input">
+      <div class="input-field" v-for="field in selectedCategoryFields" :key="field">
+        <input type="text" v-model="form[field]" :placeholder="field">
+        <div v-if="v$.form[field].$error">
+          This field is required.
+        </div>
       </div>
     </div>
-    <button class="press" :disabled="v$.form.$invalid" @click="create">Login</button>
+    <button class="press" :disabled="v$.form.$invalid" @click="create">Create</button>
+    </div>
+    
 </template>
 
+
+
+<style scoped>
+  .fields {
+      display:flex;
+      justify-content:flex-start;
+      align-items:center;
+      flex-direction: column;
+      margin:50px 150px;
+  }
+
+  .input {
+    /* make inputs 2 in a row, unlimited rows */
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: minmax(100px, auto);
+    grid-gap: 20px;
+
+     
+    
+
+  }
+
+  .input-field {
+      padding: 10px 0;
+    border-radius: 50px;
+    background-color: transparent;
+    border: 1px solid  rgba(var(--v-theme-inputText), 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
+
+  .title {
+    display:grid;
+  }
+
+  h1 {
+    color: rgb(var(--v-theme-text));
+    margin: 50px 150px 20px 150px;
+    justify-self: flex-start;
+  }
+
+  input::placeholder {
+    color:red;
+  }
+
+.press {
+   padding: 10px 60px;
+   border-radius: 50px;
+   color: rgb(var(--v-theme-background));
+}
+
+</style>
