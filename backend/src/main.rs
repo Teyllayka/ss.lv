29418adde210ae5fc735307d2b82c8ae0a331bb6
Teyllayka <teyylayt@gmail.com>
2024-06-03 19:03:02 +0300

    cors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -932,10 +932,7 @@ async fn main() -> std::io::Result<()> {
             ))
             .finish();
         let cors = Cors::default()
-            .allowed_origin("http://127.0.0.1:3000")
-            .allowed_origin("http://localhost:3000")
-            .allowed_origin("http://localhost:80")
-            .allowed_origin("https://api-12dpdsprogis.kvalifikacija.rvt.lv/")
+            .allow_any_origin()
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
