15326486a197ad6f1d4b1d9a7b1b655e30679f99
Teyllayka <teyylayt@gmail.com>
2024-06-06 01:08:41 +0300

    fixes

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -70,16 +70,16 @@ watchEffect(() => {
       <div v-if="menu" class="burger-menu">
         <div class="burger-button" @click="openMenu"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"/></svg></div>
         <div class="items">
-          <router-link to="/home" >Adee</router-link>
-        <router-link to="/home" class="link">Home</router-link>
-        <router-link to="/create" class="link">Create</router-link>
-        <router-link :to="logedIn ? '/me' : '/register'">
+        <router-link to="/home" @click="openMenu">Adee</router-link>
+        <router-link to="/home" class="link" @click="openMenu">Home</router-link>
+        <router-link to="/create" class="link" @click="openMenu">Create</router-link>
+        <router-link :to="logedIn ? '/me' : '/register'" @click="openMenu">
             Account
           </router-link>
-          <router-link to="/adverts" >
+          <router-link to="/adverts" @click="openMenu">
             Search
           </router-link>
-          <router-link to="/bookmarks" >
+          <router-link to="/bookmarks" @click="openMenu">
             Bookmarks
           </router-link>
         </div>
