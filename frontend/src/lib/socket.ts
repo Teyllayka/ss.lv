d36bffec1393af80b8db7355ae349907628f2924
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2025-05-16 02:05:20 +0300

    test

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,4 +1,14 @@
 import { io } from "socket.io-client";
 import { chatUrl } from "./consts";
 
-export const socket = io(chatUrl);
+
+
+
+export const socket = chatUrl === "https://ad-ee.tech/chat" ? io(chatUrl, {
+  path: `${chatUrl}/socket.io`,  
+  transports: ["polling", "websocket"], 
+  secure: true,                    
+}) : io(chatUrl);
+
+
+
