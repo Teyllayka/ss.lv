6ecc6a93cac00122be770a4aa62cbf2e0cf625f1
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2025-05-20 16:28:49 +0300

    a

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -15,21 +15,19 @@ export const handle: Handle = async ({
   }, event.url.href);
 
 
-  if (event.url.pathname === "/login" || event.url.pathname === "/backend") {
-    return await resolve(event);
-  }
-
   const needsRefresh = (expiresAt && parseInt(expiresAt) < Date.now()) || !accessToken;
-  
-  if (needsRefresh) {
+
+  if(needsRefresh) {
     console.log("Access token expired or not present");
+    event.cookies.delete("accessToken", { path: "/" });
+    event.cookies.delete("expiresAt", { path: "/" });
     if (!refreshToken) {
       console.log("No refresh token available, redirecting to login");
-      clearAuthCookies(event);
+      event.cookies.delete("refreshToken", { path: "/" });
+      event.cookies.delete("userId", { path: "/" });
       throw redirect(302, "/login");
-    }
-    
-    try {
+    } else {
+         try {
       const refresh = graphql(`
         mutation refresh($refreshToken: String!) {
           refresh(refreshToken: $refreshToken) {
@@ -50,17 +48,17 @@ export const handle: Handle = async ({
           path: "/",
           httpOnly: true,
           sameSite: "strict",
-          maxAge: 60 * 100, // 100 minutes
+          maxAge: 60 * 100, 
         });
         
         event.cookies.set("refreshToken", res.data.refresh.refreshToken, {
           path: "/",
           httpOnly: true,
           sameSite: "strict",
-          maxAge: 60 * 180, // 3 hours
+          maxAge: 60 * 180,
         });
         
-        const newExpiresAt = Date.now() + 100 * 60 * 1000; // 100 minutes in ms
+        const newExpiresAt = Date.now() + 100 * 60 * 1000;
         event.cookies.set("expiresAt", newExpiresAt.toString(), {
           path: "/",
         });
@@ -73,12 +71,74 @@ export const handle: Handle = async ({
       }
     } catch (error) {
       console.error("Token refresh failed", error);
-      clearAuthCookies(event);
+      event.cookies.delete("refreshToken", { path: "/" });
+      event.cookies.delete("userId", { path: "/" });
       throw redirect(302, "/login");
     }
-  } else {
-    setSession(event, { accessToken });
+    }
   }
+
+
+
+  
+  // if (needsRefresh) {
+  //   console.log("Access token expired or not present");
+  //   if (!refreshToken) {
+  //     console.log("No refresh token available, redirecting to login");
+  //     clearAuthCookies(event);
+  //     throw redirect(302, "/login");
+  //   }
+    
+  //   try {
+  //     const refresh = graphql(`
+  //       mutation refresh($refreshToken: String!) {
+  //         refresh(refreshToken: $refreshToken) {
+  //           refreshToken
+  //           accessToken
+  //         }
+  //       }
+  //     `);
+
+  //     let res = await refresh.mutate({ refreshToken }, { event });
+  //     console.log("Refresh response", res);
+
+  //     if (!res.errors && res.data?.refresh) {
+  //       console.log("Refresh success", res.data.refresh);
+  //       accessToken = res.data.refresh.accessToken;
+        
+  //       event.cookies.set("accessToken", accessToken, {
+  //         path: "/",
+  //         httpOnly: true,
+  //         sameSite: "strict",
+  //         maxAge: 60 * 100, // 100 minutes
+  //       });
+        
+  //       event.cookies.set("refreshToken", res.data.refresh.refreshToken, {
+  //         path: "/",
+  //         httpOnly: true,
+  //         sameSite: "strict",
+  //         maxAge: 60 * 180, // 3 hours
+  //       });
+        
+  //       const newExpiresAt = Date.now() + 100 * 60 * 1000; // 100 minutes in ms
+  //       event.cookies.set("expiresAt", newExpiresAt.toString(), {
+  //         path: "/",
+  //       });
+        
+  //       setSession(event, { accessToken });
+  //     } else {
+  //       console.log("Refresh error", res.errors);
+  //       clearAuthCookies(event);
+  //       throw redirect(302, "/login");
+  //     }
+  //   } catch (error) {
+  //     console.error("Token refresh failed", error);
+  //     clearAuthCookies(event);
+  //     throw redirect(302, "/login");
+  //   }
+  // } else {
+  //   setSession(event, { accessToken });
+  // }
 
   return await resolve(event);
 
